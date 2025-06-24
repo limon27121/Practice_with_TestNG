2a4006de96885c4dfdb492ffb636d1fded3868b5
limon27121 <moontaser009@gmail.com>
2025-06-24 16:03:51 +0600

    added data_provider pcakage with data provider method

---
+++ src/test/java/TestNG/Dataproviders_and_parallel_testing/DataProvider_Demo.java
@@ -0,0 +1,58 @@
+package TestNG.Dataproviders_and_parallel_testing;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DataProvider_Demo {
+    WebDriver driver;
+    @BeforeClass
+    void Setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test(dataProvider = "dp1")
+    void Test_Login(String email,String password) throws InterruptedException {
+        driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        Thread.sleep(2000);
+        boolean status=driver.findElement(By.xpath("//h2[normalize-space()='My Account']")).isDisplayed();
+        if (status==true){
+            driver.findElement(By.xpath("//a[@class='list-group-item'][normalize-space()='Logout']")).click();
+            Assert.assertTrue(true);
+        }
+        else{
+            Assert.fail();
+        }
+
+    }
+
+    @AfterClass
+    void down(){
+        driver.close();
+    }
+
+    @DataProvider(name = "dp1")
+    Object[][] login_data(){
+       Object[][] userCredentials = {
+                {"user1@example.com", "password123"},
+                {"user2@example.com", "pass456"},
+                {"user3@example.com", "abc789"},
+                {"user4@example.com", "mypassword"},
+                {"moontaser009@gmail.com", "limon123"}
+        };
+        return userCredentials;
+    }
+}
